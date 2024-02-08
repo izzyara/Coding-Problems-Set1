17f76dc580012f74967a5ada974a50d745e1e611
izzyara <izzybzb@gmail.com>
2024-02-08 14:52:10 -0500

    coding problems

---
+++ example2.java
@@ -0,0 +1,7 @@
+public class example2
+{
+        public static void main (String[] args)
+    {
+    System.out.println("*****************************************\n*\tSue's swimwear\t\t\t*\n*\tSwimWithSue@swimwear.com\t*\n*****************************************");
+    }
+}
---
+++ example3.java
@@ -0,0 +1,8 @@
+
+public class example3
+{
+        public static void main (String[] args)
+    {
+    System.out.println("Izzy Arabe\n\nSong:\"My freezing breath, frozen memories\"\nPoem:\"Let there be new flowering\"");
+    }
+}
